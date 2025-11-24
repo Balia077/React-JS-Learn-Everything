c9435ac03a097fa83d899e60d79239a46db4d02b
Balia077 <balaramd894@gmail.com>
2025-11-24 11:44:09 +0530

    Named import, default imprt and named export, default export.

---
+++ Basics/Import and Export/app.js
@@ -0,0 +1,6 @@
+// Named Export
+export var a = 10;
+
+// Default Export
+const user = "Balaram";
+export default user;
---
+++ Basics/Import and Export/script.js
@@ -0,0 +1,8 @@
+// Named Import
+import {a} from './app.js';
+console.log(a);     //10
+
+// Default Import
+import user from './app.js';
+console.log(user);  //Balaram
+
