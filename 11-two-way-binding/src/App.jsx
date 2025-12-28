1c77042f26b988d81be7cbfa82ab77f74bf820fc
Balia077 <balaramd894@gmail.com>
2025-12-28 13:07:22 +0530

    feat: initialize two-way binding project with React and Vite
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Included Vite logo SVG in the public directory.
    - Created App.css for styling the application.
    - Implemented App.jsx with a simple form for two-way data binding.
    - Added React logo SVG in the assets directory.
    - Created index.css for global styles.
    - Set up main.jsx to render the App component.
    - Configured Vite with vite.config.js for React support.

---
+++ 11-two-way-binding/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [title, setTitle] = useState('')
+
+  const formSubmit = (e) => {
+    e.preventDefault()
+    console.log('form submitted by', title)
+    setTitle('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        formSubmit(e)
+      }}>
+        <input type="text" placeholder='Enter your name' 
+          value={title}
+          onChange={(e)=>{
+            setTitle(e.target.value)
+          }}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
