4f98c9ea6ccd634aba4ad171873fb73cfce5d9e8
Balia077 <balaramd894@gmail.com>
2026-01-03 18:23:14 +0530

    implement navigation buttons in Navbar2 component for dynamic routing

--- 18-routing-advanced/src/components/Navbar2.jsx
+++ 18-routing-advanced/src/components/Navbar2.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar2 = () => {
 
-    
+    const navigate = useNavigate()
 
-  return (
-    <div>
-      <button className='font-medium bg-emerald-700 px-5 py-2 rounded'>Back To Home Page</button>
-    </div>
-  )
+    return (
+        <div>
+            <button
+                onClick={()=>{
+                    navigate('/')
+                }}
+                className='font-medium bg-emerald-700 m-2 cursor-pointer active:scale-95 px-5 py-2 rounded'>Back To Home Page
+            </button>
+            <button
+                onClick={()=>{
+                    navigate(-1)
+                }}
+                className='font-medium bg-emerald-700 m-2 cursor-pointer active:scale-95 px-5 py-2 rounded'>Back
+            </button>
+            <button
+                onClick={()=>{
+                    navigate(+1)
+                }}
+                className='font-medium bg-emerald-700 m-2 cursor-pointer active:scale-95 px-5 py-2 rounded'>Next
+            </button>
+        </div>
+    )
 }
 
 export default Navbar2
