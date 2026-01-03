748f23167782b54568432a294ec5b51e6825c18b
Balia077 <balaramd894@gmail.com>
2026-01-03 18:14:43 +0530

    add Navbar2 component for enhanced navigation

---
+++ 18-routing-advanced/src/components/Navbar2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar2 = () => {
+
+    
+
+  return (
+    <div>
+      <button className='font-medium bg-emerald-700 px-5 py-2 rounded'>Back To Home Page</button>
+    </div>
+  )
+}
+
+export default Navbar2
